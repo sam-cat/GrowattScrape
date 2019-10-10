f9f394781e62fc18fabbe3a3c61aeb65ce3bf034
sam-cat <Sam.Catchesides@EUROPA-GROUP.CO.UK>
2019-10-10 10:29:28 +0100

    test commit 3

--- Scrape Growatt.py
+++ Scrape Growatt.py
@@ -6,7 +6,7 @@ import sys
 import json
 import datetime
 
-## A comment to make source control kick in...
+## A comment to make source control kick in....
 
 ## Setup a session for this work
 Growatt_session = requests.session()
